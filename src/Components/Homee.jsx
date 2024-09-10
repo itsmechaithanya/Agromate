1fea9d1ab12bf28d8d08419cb714c25e8d3dac22
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-09-10 15:03:25 +0530

    first commit

--- src/Components/Homee.jsx
+++ src/Components/Homee.jsx
@@ -7,24 +7,24 @@ function Homee() {
   return (
     <div>
       <div className='lg:flex'>
-        <div>
-          <div className='text-center mt-[5vh]'>
-            <h1 className='font-[Futura-Bold] text-[4.5vh] uppercase'>Mobile-Operated Agricultural Motor System</h1>
+        <div className='lg:w-[60vw] '>
+          <div className='text-center mt-[5vh] lg:text-start lg:pl-5 '>
+            <h1 className='font-[Futura-Bold] text-[4.5vh] lg:text-[4vw] uppercase'>Mobile-Operated Agricultural Motor System</h1>
           </div>
-          <div className='text-center px-5 mt-[2vh]'>
+          <div className='text-center px-5 mt-[2vh] '>
             <p>Control Your Motor Pump with a Simple Missed Call, Anytime, Anywhere!</p>
           </div>
         </div>
-        <div>
+        <div className='lg:w[40vw]'>
           <div className='flex justify-center'>
-            <img className='w-[90vw]' src={hero} alt="" />
+            <img className='w-[90vw] lg:w-[40vw]' src={hero} alt="" />
           </div>
         </div>
       </div>
       <div>
-        <h1 className='font-[Futura-Bold] mt-[2vh] px-5 text-lg font-bold'>Effortlessly manage your motor pump, saving time, energy, and resources.</h1>
+        <h1 className='font-[Futura-Bold] mt-[2vh] px-5 text-md uppercase'>Effortlessly manage your motor pump, saving time, energy, and resources.</h1>
       </div>
-      <div className='flex justify-center mt-[3vh]'>
+      <div className='flex justify-center mt-[5vh]'>
         <img className='w-[90vw]' src={buff} alt="" />
       </div>
     </div>
