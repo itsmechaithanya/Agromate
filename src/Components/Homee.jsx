8b5982a6154d8de25f5978a8c55ab3c697fea632
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-09-10 16:55:06 +0530

    first commit

--- src/Components/Homee.jsx
+++ src/Components/Homee.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import hero from '../assets/hero.png'
 import buff from '../assets/buff.png'
+import AgromateKisan from '../assets/AgromateKisan.png'
+import AgromatePlus from '../assets/AgromatePlus.png'
 
 
 function Homee() {
@@ -38,6 +40,60 @@ function Homee() {
       </div>
       <div className='px-5 mt-[8vh]'>
         <h1 className='font-semibold'>Agromate lets you control your water motor pump directly from your mobile phone, no matter where you are. With just a missed call from your smartphone, you can start or stop the pump effortlessly.</h1>
+      </div>
+      <div className='px-5 mt-[2vh]'>
+        <p className='font-light text-[#323232]' >This groundbreaking technology offers more than just convenience. By using Agromate, you cut down on physical visits to your motor pump, leading to significant savings in electricity, time, and water resources.</p>
+      </div>
+      <div className='pl-5 mt-[3vh]'>
+        <p className=' uppercase border-b-[1px] w-fit border-zinc-500 pb-1'>installation guide</p>
+      </div>
+      <div className='flex justify-center mt-[10vh]'>
+        <img className='w-[70vw]' src={AgromateKisan} alt="" />
+      </div>
+      <div className='text-center'>
+        <h1 className='font-semibold text-lg capitalize text-zinc-600'>Agromate Kisan</h1>
+      </div>
+      <div className='px-8 mt-[3vh]'>
+        <p className='font-light  ' >Agromate Kisan, a farmer-friendly device with remote motor control. It simplifies motor management, allowing ON/OFF with a missed call, using LED indicators instead of an LCD screen. It also offers manual ON/OFF for flexibility.</p>
+      </div>
+      <div className='pl-8 mt-[2vh]'>
+        <p className=' uppercase border-b-[1px] w-fit border-zinc-500 pb-1'>Know more</p>
+      </div>
+      <div className='flex justify-center mt-[10vh]'>
+        <img className='w-[70vw]' src={AgromatePlus} alt="" />
+      </div>
+      <div className='text-center'>
+        <h1 className='font-semibold text-lg capitalize text-zinc-600'>Agromate Kisan</h1>
+      </div>
+      <div className='px-8 mt-[3vh]'>
+        <p className='font-light  ' >Agromate Plus is a telemetric device for remote motor control. Designed for farmers, it offers remote motor management through a simple missed call, with an LCD screen for status display, and a manual ON/OFF option.</p>
+      </div>
+      <div className='pl-8 mt-[2vh]'>
+        <p className=' uppercase border-b-[1px] w-fit border-zinc-500 pb-1'>Know more</p>
+      </div>
+      <div className='flex justify-center mt-[10vh]'>
+        <img className='w-[70vw]' src={AgromatePlus} alt="" />
+      </div>
+      <div className='text-center'>
+        <h1 className='font-semibold text-lg capitalize text-zinc-600'>Agromate Kisan</h1>
+      </div>
+      <div className='px-8 mt-[3vh]'>
+        <p className='font-light  ' >Agromate Plus is a telemetric device for remote motor control. Designed for farmers, it offers remote motor management through a simple missed call, with an LCD screen for status display, and a manual ON/OFF option.</p>
+      </div>
+      <div className='pl-8 mt-[2vh]'>
+        <p className=' uppercase border-b-[1px] w-fit border-zinc-500 pb-1'>Know more</p>
+      </div>
+      <div className='flex justify-center mt-[10vh]'>
+        <img className='w-[70vw]' src={AgromatePlus} alt="" />
+      </div>
+      <div className='text-center'>
+        <h1 className='font-semibold text-lg capitalize text-zinc-600'>Agromate Kisan</h1>
+      </div>
+      <div className='px-8 mt-[3vh]'>
+        <p className='font-light  ' >Agromate Plus is a telemetric device for remote motor control. Designed for farmers, it offers remote motor management through a simple missed call, with an LCD screen for status display, and a manual ON/OFF option.</p>
+      </div>
+      <div className='pl-8 mt-[2vh]'>
+        <p className=' uppercase border-b-[1px] w-fit border-zinc-500 pb-1'>Know more</p>
       </div>
     </div>
   )
