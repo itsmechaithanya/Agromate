f530309d1d206867cf56cfb4493f72ae11536d05
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-10-06 11:59:32 +0530

    first commit

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
-
-function ContactUs() {
+import React, { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  ColorPicker,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Rate,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from 'antd';
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+const ContactUs = () => {
+  const [componentDisabled, setComponentDisabled] = useState(true);
   return (
-    <div className='h-screen w-screen bg-rose-50'>ContactUs</div>
-  )
-}
-
+    <div className='h-screen w-screen flex justify-center items-center'>
+     
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        
+        style={{
+          maxWidth: 600,
+        }}
+      >
+       
+        <Form.Item label="Email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password">
+          <Input />
+        </Form.Item>
+     
+       
+       <Button>Submit</Button>
+      
+      
+      </Form>
+    </div>
+  );
+};
 export default ContactUs
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,8 +1,27 @@
+import { Button, Card } from 'antd'
 import React from 'react'
-
+import Image from "../assets/G.png"
 function Shop() {
   return (
-    <div>Shop</div>
+    <div className="product m-20">
+     
+
+      <div className="product-info">
+        <Card
+      size="small"
+      title="Small size card"
+      extra={<a href="#">More</a>}
+      style={{
+        width: 300,
+      }}
+      cover={<img src={Image} alt="Product Image" />}
+    > 
+      <h3 className='text-xl pb-4'>Agromate Plus</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error recusandae temporibus, neque quos cumque accusantium tempore aperiam suscipit unde! Corrupti, provident sit! Corporis nihil a, id eaque labore cum eos!</p>
+     <div className='flex pt-4 justify-between items-center'> <p>Rs. 10</p><Button>Buy now</Button></div>
+    </Card>
+      </div>
+    </div>
   )
 }
 
