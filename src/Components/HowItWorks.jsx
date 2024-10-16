bcf7fd98e69a19b389d8d75ccdc1229ad98e7849
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-10-16 15:58:02 +0530

    first commit

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -3,6 +3,7 @@ import step1 from '../assets/step1.jpg'
 import step2 from '../assets/step2.jpg'
 import step3 from '../assets/step3.jpg'
 import DOL from '../assets/DOL.jpg'
+import Star from '../assets/Star.jpg'
 
 function HowItWorks() {
   return (
@@ -10,7 +11,7 @@ function HowItWorks() {
             <div className=' h-[50vh] flex items-center pl-[5vw]'>
         <h1 className='text-[#2F2F69] capitalize text-[4.5vw] font-bold '>How it works ?<br />Guide<i class="ri-arrow-right-down-line"></i></h1>
       </div>
-      <div className='px-[5vw] bg-slate-100 pt-[5vw]  rounded-t-[5vw]'>
+      <div className='px-[5vw] bg-slate-100 pt-[5vw] pb-[1vw] rounded-t-[5vw]'>
         <div><h1 className='text-xl'>Follow the steps below <i class="ri-arrow-down-s-line"></i></h1></div>
         <div className='flex flex-col justify-center items-center gap-[2vw] pt-[3vw]'>
           <img className='h-[90vh] w-[80vw]' src={step1} alt="" />
@@ -21,6 +22,16 @@ function HowItWorks() {
         <h1 className='text-xl pb-[3vw]'>Follow the below diagrams for connecting with DOL starter <i class="ri-arrow-down-s-line"></i></h1>
         </div>
       <img src={DOL} alt="" />
+      <div className='pt-[5vw]'>
+        <h1 className='text-xl pb-[3vw]'>Follow the below diagrams for connecting with Star-Delta starter <i class="ri-arrow-down-s-line"></i></h1>
+        </div>
+      <img src={Star} alt="" />
+      <div className='flex justify-center pt-[3vw]'>
+        <div className='w-[90vw] border-[.5px] border-[#0000008b]'></div>
+      </div>
+      <div className='my-[3vh] text-center'>
+        <p className='font-light text-xs'>Copyright © 2024 Agromate All Right Reserved.</p>
+      </div>
       </div>
     </div>
   )
