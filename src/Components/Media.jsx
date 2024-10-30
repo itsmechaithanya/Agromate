e4292cd9da6a8ecdbb9b790830b8be4eadef2ee2
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-10-30 11:16:04 +0530

    first commit

--- src/Components/Media.jsx
+++ src/Components/Media.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 function Media() {
   return (
-    <div>Media</div>
-  )
+    <div className='pt-[11vh]'>
+      <div className='h-[50vh] flex items-center pl-[5vw]'>
+        <h1 className='capitalize text-[4.5vw] font-bold text-[#2F2F69]'>
+          Media, Newspapers  <br /> and more<i className="ri-arrow-right-down-line"></i>
+        </h1>
+      </div>
+      <div className='px-[5vw] bg-slate-100 pt-[5vw] pb-[1vw] rounded-t-[5vw]'>
+        <h1 className='mb-[3vh] font-medium text-[1.8vw]'>5th October 2018: HMTV Agri Program</h1>
+        <div className='video w-[90vw] h-[90vh]'>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/sgV_pGWl2Hs"
+            title="Agromate Media Coverage"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <h1 className='mb-[3vh] mt-[7vh]  font-medium text-[1.8vw]'>21st April 2015: TV5 Annapurna Program</h1>
+        <div className='video w-[90vw] h-[90vh]'>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/hyn39cypGGo"
+            title="Agromate Media Coverage"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className='video w-[90vw] h-[90vh] mt-[3vh]'>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/TkyRhznQgBU"
+            title="Agromate Media Coverage"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <h1 className='mb-[3vh] mt-[7vh] font-medium text-[1.8vw]'>11th April 2015: TV5 News - TV5 Business Leaders 2015 Agromate</h1>
+        <div className='video w-[90vw] h-[90vh]'>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/VBA5HwoKiow"
+            title="Agromate Media Coverage"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Media
+export default Media;
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,19 +1,25 @@
 import { Button, Card } from 'antd'
 import React from 'react'
-import Image from "../assets/G.png"
+import AgromatePlus from "../assets/AgromatePlus.png"
+
 function Shop() {
   return (
     <div className="pt-[11vh]">
       <div className='h-[50vh] flex items-center pl-[5vw]'>
         <h1 className='capitalize text-[4.5vw] font-bold text-[#2F2F69] '>We offer you <br /> the best <i class="ri-arrow-right-down-line"></i></h1>
       </div>
-    <div className='bg-slate-100 px-[3vw] pt-[4vw] rounded-t-[5vw]'>
+    <div className='bg-slate-100 px-[3vw] pt-[4vw] rounded-t-[5vw] pb-[4vw]'>
     <div className='flex justify-end'><h1 className='bg-slate-300 px-[2vw] py-[1vw] rounded-full'>0 items in <i className="ri-shopping-cart-line font-medium"></i> </h1> </div>
       <div className='my-[5vh]'>
         <h1>Please take a look at the product list displayed below. You can choose to make your purchases online, and we will ensure swift delivery to your specified address. If you need installation support, we offer remote assistance, and you can access informative installation videos in the documents section.</h1>
       </div>
       <div>
-        <div className='h-[10vh] w-[20vw] bg-slate-200'></div>
+        <div className='h-[60vh] w-[30vw] bg-gray-200 rounded-xl flex flex-col items-center'>
+          <div className='flex justify-center items-center pt-[4vh]'>
+          <img className='h-[30vh]' src={AgromatePlus} alt="" />
+          </div>
+          <h1 className='mt-[2vh]'>AgromatePlus</h1>
+        </div>
       </div>
     </div>
     </div>
